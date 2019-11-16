dda0cec6a2ee50ccdaadde7f73977874c6019117
Admi <admi@MacBook-Pro-Admin.local>
2019-11-16 17:46:31 +0200

    option to delete item from list

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -10,12 +10,27 @@ import './app.css'
 export default class App extends Component {
 
     state = {
-
         todoData: [
             {label: 'Drink Water', important: false, id: 0},
             {label: 'Make', important: true, id: 1},
             {label: 'Learn', important: false, id: 2}
         ]
+    };
+
+    deleteItem=(id)=>{
+        console.log('id to delete:', id);
+        this.setState(({todoData})=>{
+            const idx=todoData.findIndex((el)=>el.id===id);
+            //todoData.splice(idx,1)
+
+            const arrayBefore=todoData.slice(0,idx);
+            const arrayAfter=todoData.slice(idx+1);
+            const newArray= [...arrayBefore,...arrayAfter];
+            return{
+                todoData:newArray
+            }
+            }
+        )
     };
 
     render() {
@@ -27,9 +42,7 @@ export default class App extends Component {
                     <ItemStatusFilter/>
                 </div>
                 <TodoList todos={this.state.todoData}
-                          onDeleted={(id) => {
-                              console.log('id to delete:', id)
-                          }}/>
+                          onDeleted={this.deleteItem}/>
             </div>
         );
     }
