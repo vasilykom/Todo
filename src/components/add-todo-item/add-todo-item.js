a44903a067589588218e059cdd6dfd7238c48cf2
Admi <admi@MacBook-Pro-Admin.local>
2019-11-18 18:21:12 +0200

    create input for todo

--- src/components/add-todo-item/add-todo-item.js
+++ src/components/add-todo-item/add-todo-item.js
@@ -3,23 +3,39 @@ import './add-todo-item.css'
 
 export default class AppTodoItem extends Component {
 
+    state={
+        label:''
+    };
+
+    onLabelChange = (e) => {
+     this.setState({
+         label:e.target.value
+     })
+    };
+
+    onSubmit=(e)=>{
+        e.preventDefault();
+        this.props.onItemAdded(this.state.label);
+    };
 
     render() {
         //const {onCreated} = this.props;
         return (
 
-            <div className='add-todo-item'>
-                {/*<input type="text"
+            <form className='add-todo-item d-flex'
+            onSubmit={this.onSubmit}>
+                <input type="text"
                        className="form-control"
-                       placeholder="Add a new todo"/>*/}
+                       onChange={this.onLabelChange}
+                       placeholder="Add a new todo"/>
                 <button
                     className='btn btn-outline-secondary'
                     // onClick={onCreated}
-                    onClick={() => this.props.onItemAdded('Hello from items')}
+                    //onClick={() => this.props.onItemAdded('Hello from items')}
                 >
                     Add item
                 </button>
-            </div>
+            </form>
 
         );
     }
