2d97fe04c92e46c07fd939bef3fead002cfe67a1
Admi <admi@MacBook-Pro-Admin.local>
2019-11-18 19:09:52 +0200

    create input for todo finish 2

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -5,7 +5,7 @@ const AppHeader = ({todoCount, doneCount}) => {
     return (
         <div className='app-header d-flex'>
             <h1>My Todo list</h1>
-            <h2>{todoCount}more to do, done {doneCount}</h2>
+            <h2>{todoCount} more to do, done {doneCount}</h2>
         </div>
 
 
